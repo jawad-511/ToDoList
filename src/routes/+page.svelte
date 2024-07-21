58239f288f0e0ee622ec531143a697ac388985a0
Jawad Alotibi <s444003256@st.uqu.edu.sa>
2024-07-21 21:50:11 +0300

    The Header

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,19 @@
-<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+<script lang="ts">
+	import dayjs from 'dayjs';
+	import 'dayjs/locale/ar';
 
-<div class="container h-full mx-auto flex justify-center items-center">
-	<div class="space-y-5">
-		<h1 class="h1">Let's get cracking bones!</h1>
-		<p>Start by exploring:</p>
-		<ul>
-			<li><code class="code">/src/routes/+layout.svelte</code> - barebones layout</li>
-			<li><code class="code">/src/app.postcss</code> - app wide css</li>
-			<li>
-				<code class="code">/src/routes/+page.svelte</code> - this page, you can replace the contents
-			</li>
-		</ul>
+	dayjs.locale('ar');
+
+	let period = dayjs().format('a') =='م' ? 'مساء' : 'صباح';
+</script>
+<div class="p-16">
+	<header class='flex justify-between'>
+		<div>
+		<h1 class="text-3xl">{period} الخير👋</h1>
+		<h2 class="text-surface-900/50 mt-2">	
+				{dayjs().format("اليوم ,dddd D MMMM YYYY")}
+		</h2>
 	</div>
+		<div class='bg-white w-16 h-16'></div>
+	</header>
 </div>
