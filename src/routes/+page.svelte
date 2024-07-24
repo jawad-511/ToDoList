91b59d3290be30a849e21bcf4724a1e8a894e4c7
Jawad Alotibi <s444003256@st.uqu.edu.sa>
2024-07-24 06:04:41 +0300

    Last change in to do list

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,11 @@
   import dayjs from "dayjs";
 
   import { tasks } from "$lib/stores/tasks";
+ 
 </script>
+<svelte:head>
+  <title>To Do List</title>
+</svelte:head>
 
 <div
   class="p-2 sm:p-16 md:p-24 lg:p-32 flex flex-col gap-10 max-w-screen-lg mx-auto"
