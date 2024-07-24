d424c24f025b1d815e05fc6898409820cbeb810c
Jawad Alotibi <s444003256@st.uqu.edu.sa>
2024-07-24 03:03:00 +0300

    Adding modal

--- src/lib/TaskListItems.svelte
+++ src/lib/TaskListItems.svelte
@@ -1,41 +1,62 @@
 <script lang="ts">
-    import dayjs from "dayjs";
-    import { tasks } from "$lib/stores/tasks";
-    import relativeTime from "dayjs/plugin/relativeTime";
-  
-    dayjs.extend(relativeTime);
+  import dayjs from "dayjs";
+  import { tasks } from "$lib/stores/tasks";
+  import relativeTime from "dayjs/plugin/relativeTime";
 
-   export let doneTasks:Boolean;
-  </script>
+  import { getModalStore, type ModalSettings } from "@skeletonlabs/skeleton";
+
+  dayjs.extend(relativeTime);
+
+  const modalStore = getModalStore();
+
+  function confirmDelete(task : Task) {
+    const modal: ModalSettings = {
+      type: "confirm",
+      // Data
+      title: "هل تود حقًا حذف المهمة؟",
+      body:`عزيزي المستخدم,نأسف لإبلاغك أنه بمجرد حذف المهمة: "${task.title}" ، لن يكون بإمكانك استعادتها. يرجى التأكد من أنك ترغب فعلاً في حذفها قبل الاستمرار.`,
+      // TRUE if confirm pressed, FALSE if cancel pressed
+      response: (r: boolean) => console.log("response:", r),
+      buttonTextCancel : ('إلغاء'),
+      buttonTextConfirm : ('تأكيد'),
+      
+    
+    };
+    modalStore.trigger(modal);
+  }
+
+  export let doneTasks: Boolean;
+</script>
 
 {#each $tasks as task}
-    {#if !task.isDone == doneTasks}
-      <li class="bg-white p-2 rounded-md flex justify-between items-center">
-        <div>
-          <input bind:checked={task.isDone} class="checkbox" type="checkbox" />
-          <span class="mr-1">{task.title}</span>
-        </div>
-        <div class="flex gap-2">
-          <button
-            class="btn bg-[#151d2f] text-white hover:bg-slate-800 rounded-md p-2 w-40"
-          >
-            {dayjs(task.assignedDate).format("hh:mm")}
-          </button>
+  {#if !task.isDone == doneTasks}
+    <li class="bg-white p-2 rounded-md flex justify-between items-center">
+      <div>
+        <input bind:checked={task.isDone} class="checkbox" type="checkbox" />
+        <span class="mr-1">{task.title}</span>
+      </div>
+      <div class="flex gap-2">
+        <button
+          class="btn bg-[#151d2f] text-white hover:bg-slate-800 rounded-md p-2 w-40"
+        >
+          {dayjs(task.assignedDate).format("hh:mm")}
+        </button>
 
-          <button
-            class="btn bg-[#151d2f] text-white hover:bg-slate-800 rounded-md p-2"
+        <button
+          on:click={() => confirmDelete(task)}
+          class="btn bg-[#151d2f] text-white hover:bg-slate-800 rounded-md p-2"
+        >
+          <svg
+            class="w-6"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            ><path
+              fill="currentColor"
+              d="M9.808 17h1V8h-1zm3.384 0h1V8h-1zM6 20V6H5V5h4v-.77h6V5h4v1h-1v14z"
+            /></svg
           >
-            <svg
-              class="w-6"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              ><path
-                fill="currentColor"
-                d="M9.808 17h1V8h-1zm3.384 0h1V8h-1zM6 20V6H5V5h4v-.77h6V5h4v1h-1v14z"
-              /></svg
-            >
-          </button>
-        </div>
-      </li>
-    {/if}
-  {/each}
+        </button>
+      </div>
+    </li>
+  {/if}
+{/each}
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,20 @@
 <script lang="ts">
-	import '../app.postcss';
+  import "../app.postcss";
 
-	// Floating UI for Popups
-	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
-	import { storePopup } from '@skeletonlabs/skeleton';
-	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+  // Floating UI for Popups
+  import {
+    computePosition,
+    autoUpdate,
+    flip,
+    shift,
+    offset,
+    arrow,
+  } from "@floating-ui/dom";
+  import { storePopup } from "@skeletonlabs/skeleton";
+  storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+  import { initializeStores, Modal } from "@skeletonlabs/skeleton";
+
+  initializeStores();
 </script>
-
+<Modal />
 <slot />
